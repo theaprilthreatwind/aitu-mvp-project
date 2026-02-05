36056cea39a094c8c409467a5847ddfbb9bfb073
Ubuntu-beibarys <shaldykenovbeibarysryspekuly@gmail.com>
2026-02-05 12:39:14 +0500

    удален класс animate-boundary в loadingSkeleton

--- frontend/src/shared/loading-skeleton/loading-skeleton.jsx
+++ frontend/src/shared/loading-skeleton/loading-skeleton.jsx
@@ -1,10 +1,9 @@
 export function LoadingSkeleton() {
   return (
     <div className="w-full gap-x-2 flex justify-center items-center h-screen text-2xl">
-      <div className="w-5 bg-[#91b6d9] animate-pulse h-5 rounded-full animate-bounce"></div>
-      <div className="w-5 animate-pulse h-5 bg-[#698fb8] rounded-full animate-bounce"></div>
-      <div className="w-5 h-5 animate-pulse bg-[#5674cc] rounded-full animate-bounce"></div>
+      <div className="w-5 bg-[#91b6d9] animate-pulse h-5 rounded-full"></div>
+      <div className="w-5 animate-pulse h-5 bg-[#698fb8] rounded-full"></div>
+      <div className="w-5 h-5 animate-pulse bg-[#5674cc] rounded-full"></div>
     </div>
   );
 }
-  
