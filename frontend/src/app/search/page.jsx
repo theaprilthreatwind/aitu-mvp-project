a366dbc1e068bcc06bd664aa6629f73c1b96e681
Ubuntu-beibarys <shaldykenovbeibarysryspekuly@gmail.com>
2026-02-11 13:35:39 +0500

    правки в search

--- frontend/src/app/search/page.jsx
+++ frontend/src/app/search/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic"
 
 import React, { use } from "react";
 import { useState } from "react";
@@ -6,7 +7,6 @@ import "./App.css";
 import Link from "next/link";
 import { fetchRestaurantList } from "@/shared";
 
-export const dynamic = "force-dynamic"
 
 const Search = () => {
   const [search, setSearch] = useState("");
